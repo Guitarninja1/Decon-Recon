6426cfa8f0ee61093a46c4077ef8be2a82dc294c
Erick Askim <erickaskim@gmail.com>
2021-10-20 11:43:52 -0700

    m3v9

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,21 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
+  static getInitialProps(ham) {
+    const h = ham.renderPage
+    const sheet = new ServerStyleSheet();
+    const page = h(
+      function (App) { 
+        return function (props) { 
+          const a = <App {...props} />
+          return sheet.collectStyles(a)
+        }
+      }
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  };
   render() {
     return (
       <Html lang="en-US">
