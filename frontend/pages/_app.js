7a43baaef892b617d9fbc5a95551b1605e6ae44b
Erick Askim <erickaskim@gmail.com>
2021-10-20 10:52:56 -0700

    m3v8

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,18 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import PropTypes from 'prop-types';
 import Page from '../components/Page';
+import NProgress from 'nprogress'
+import Router from 'next/router'
+import '../components/styles/nprogress.css'
+
+const rcs = 'routeChangeStart';
+const rcc = 'routeChangeComplete';
+const rce = 'routeChangeError';
+const re = Router.events;
+re.on(rcs, () => NProgress.start())
+re.on(rcc, () => NProgress.done())
+re.on(rce, () => NProgress.done())
+
 
 function MyApp(ham) {
   const p = ham.pageProps;
