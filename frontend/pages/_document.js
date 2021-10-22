dd38bc68987397ff04ad1964b2fad13b135c9461
Erick Askim <erickaskim@gmail.com>
2021-10-22 09:29:08 -0700

    m4v13

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,23 +3,22 @@ import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
   static getInitialProps(ham) {
-    const h = ham.renderPage
+    const h = ham.renderPage;
     const sheet = new ServerStyleSheet();
-    const page = h(
-      function (App) { 
-        return function (props) { 
-          const a = <App {...props} />
-          return sheet.collectStyles(a)
-        }
-      }
-    );
+    const page = h(function (App) {
+      return function (props) {
+        const a = <App {...props} />;
+        return sheet.collectStyles(a);
+      };
+    });
     const styleTags = sheet.getStyleElement();
     return { ...page, styleTags };
-  };
+  }
+
   render() {
     return (
       <Html lang="en-US">
-        <Head></Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
